8fa210083c5634cf2da2b7b6d6707da917086876
LTY77645243 <49172166+LTY77645243@users.noreply.github.com>
2022-06-17 15:41:50 -0400

    set up resuable logic and fontawesome

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
+import '../styles/globals.css';
+import { Navbar, Footer } from '../components';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+    <Script src="https://kit.fontawesome.com/1dcece299d.js" crossorigin="anonymous" />
+  </ThemeProvider>
+);
 
-export default MyApp
+export default MyApp;

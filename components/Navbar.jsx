7deca56ba73700650ceac40f8cc6281d0869078c
LTY77645243 <49172166+LTY77645243@users.noreply.github.com>
2022-06-19 13:47:32 -0400

    Footer development

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,6 @@ import { useRouter } from 'next/router';
 import { useTheme } from 'next-themes';
 import Image from 'next/image';
 import Link from 'next/link';
-
 import images from '../assets';
 import { Button } from '.';
 

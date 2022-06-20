09b4cb409e1b5cfc60ca08189581618e2a4ec12c
LTY77645243 <49172166+LTY77645243@users.noreply.github.com>
2022-06-20 01:42:05 -0400

    footer fixed

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,15 +4,14 @@ import images from '../assets';
 import Button from './Button';
 
 const FooterLinks = ({ heading, items }) => (
-  <div className="flex-1 justify-content items-start">
-    <h3 className="font-poppins dark:text-white text-nft-black-1 font-semibold text-ml mb-10">
+  <div className="flex-1 justify-start items-start">
+    <h3 className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xl mb-10">
       {heading}
     </h3>
     {items.map((item, index) => (
       <p
         key={index}
-        className="font-poppins dark:text-white text-nft-black-1 font-normal text-base cursor-point
-      dark:hover:text-nft-gray-1 hover:text-nft-black-1 my-3"
+        className="font-poppins dark:text-white text-nft-black-1 font-normal text-base cursor-point dark:hover:text-nft-gray-1 hover:text-nft-black-1 my-3"
       >
         {item}
       </p>
@@ -24,8 +23,8 @@ const Footer = () => {
   const { theme } = useTheme();
 
   return (
-    <Footer className="flexCenter flex-col border-t dark:border-nft-black-1 border-nft-gray-1 sm:py-8 py-16">
-      <div className="w-full minmd:w-4/5 flex-row md:flex-col sm:px-4 py-16">
+    <footer className="flexCenter flex-col border-t dark:border-nft-black-1 border-nft-gray-1 sm:py-8 py-16">
+      <div className="w-full minmd:w-4/5 flex flex-row md:flex-col sm:px-4 px-16">
         <div className="flexStart flex-1 flex-col">
           <div className="flexCenter cursor-pointer">
             <Image src={images.logo02} objectFit="contain" width={32} height={32} alt="logo" />
@@ -36,9 +35,7 @@ const Footer = () => {
           <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base mt-6">
             Get the latest updates
           </p>
-          <div className="flexBetween md:w-full minlg:w-557 w:357 mt-6
-          dark:bg-nft-black-2 bg-white border dark:border-nft-black-2 border-nft-gray-2 rounded-md"
-          >
+          <div className="flexBetween md:w-full minlg:w-557 w:357 mt-6 dark:bg-nft-black-2 bg-white border dark:border-nft-black-2 border-nft-gray-2 rounded-md">
             <input
               type="email"
               placeholder="Your Email"
@@ -56,30 +53,19 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="flexCenter w-full mt-5 border-t dark:border-nft-dark-1 border-nft-gray-1 sm:px-4 py-16">
+      <div className="flexCenter w-full mt-5 border-t dark:border-nft-black-1 border-nft-gray-1 sm:px-4 px-16">
         <div className="flexBetween flex-row w-full minmd:w-4/5 sm:flex-col mt-7">
-          <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base">
-            CrpytoKet, Inc. All rights reserved.
-          </p>
+          <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base">CrpytoKet, Inc. All Rights Reserved</p>
           <div className="flex flex-row sm:mt-4">
-            {[images.instagram, images.twitter, images.telegram, images.discord].map(
-              ({ image, index }) => (
-                <div className="mx-2 cursor-pointer" key={index}>
-                  <Image
-                    src={image}
-                    objectFit="contain"
-                    width={24}
-                    height={24}
-                    alt="social"
-                    className={theme === 'light' && 'filter-invert'}
-                  />
-                </div>
-              ),
-            )}
+            {[images.instagram, images.twitter, images.telegram, images.discord].map((image, index) => (
+              <div className="mx-2 cursor-pointer" key={`image ${index}`}>
+                <Image src={image} key={index} objectFit="contain" width={24} height={24} alt="social" className={theme === 'light' ? 'filter invert' : undefined} />
+              </div>
+            ))}
           </div>
         </div>
       </div>
-    </Footer>
+    </footer>
   );
 };
 
